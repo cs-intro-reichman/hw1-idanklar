3f5a629f48cdda18a0debe462ca4cc54da93b40a
idan klar <idanklar@idans-MacBook-Pro.local>
2024-11-06 17:04:20 +0200

    test
    java Ascend
    java Ascend
    
    clear

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,14 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+		int a = (int)(Math.random() * 100);
+		int b = (int)(Math.random() * 100);
+		int c = (int)(Math.random() * 100);
+		System.out.println(a + " " + b + " " + c);
+		int max = Math.max(a , Math.max(b, c));
+		int min = Math.min(a , Math.min(b, c));
+		int middle = a + b + c - min - max;
+		System.out.println(min + " " + middle + " " + max);
+
 	}
 }
